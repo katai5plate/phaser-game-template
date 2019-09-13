3114762db6583e9180e5172228a6ae2871dcd356
katai5plate <katai5plate.sub@gmail.com>
2019-09-14 07:34:19 +0900

    set rules

--- src/Scene/HelloScene.ts
+++ src/Scene/HelloScene.ts
@@ -1,33 +1,33 @@
 /// <reference path="../../node_modules/phaser/types/phaser.d.ts"/>
 
 export default class HelloScene extends Phaser.Scene {
-    constructor(config: Phaser.Types.Scenes.SettingsConfig) {
-        super(config);
-    }
-    preload() {
-        this.load.setBaseURL('./');
+  constructor(config: Phaser.Types.Scenes.SettingsConfig) {
+    super(config);
+  }
+  preload() {
+    this.load.setBaseURL('./');
 
-        this.load.image('sky', './assets/img/space3.png');
-        this.load.image('logo', './assets/img/phaser3-logo.png');
-        this.load.image('red', './assets/img/red.png');
-    }
-    create() {
-        this.add.image(400, 300, 'sky');
+    this.load.image('sky', './assets/img/space3.png');
+    this.load.image('logo', './assets/img/phaser3-logo.png');
+    this.load.image('red', './assets/img/red.png');
+  }
+  create() {
+    this.add.image(400, 300, 'sky');
 
-        const particles = this.add.particles('red');
+    const particles = this.add.particles('red');
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+    const emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    });
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+    const logo = this.physics.add.image(400, 100, 'logo');
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
 
-        emitter.startFollow(logo);
-    }
+    emitter.startFollow(logo);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import HelloScene from "./Scene/HelloScene"
 
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: [HelloScene]
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: [HelloScene]
 };
 
 window.onload = () => {
-    const game = new Phaser.Game(config);
+  new Phaser.Game(config);
 };
